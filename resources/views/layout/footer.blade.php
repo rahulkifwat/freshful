f19eddc0a4265c6c1dbb983225135c4e7c4cb71c
rahulkifwat <rahul@kifwat.com>
2026-06-12 12:23:26 +0530

    update code

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -65,7 +65,7 @@
 
 <!-- Main JS File -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <!-- <script src="{{ url('assets/js/main.js')}}"></script> -->
 <script src="{{ url('assets/js/slider.js')}}"></script>
--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -38,14 +38,10 @@
 
    <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-   <!-- Styles / Scripts -->
-   @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-   @vite(['resources/css/app.css', 'resources/js/app.js','resources/css/custom-bootstrap.css'])
-   @else
-   <style>
-
-
-
-   </style>
-   @endif
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+   <!-- Custom Styles -->
+   <link rel="stylesheet" href="{{ url('assets/css/custom-bootstrap.css') }}">
+   <link rel="stylesheet" href="{{ url('assets/css/app.css') }}">
  </head>
